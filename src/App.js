4863dc1a4091190cf02a66e5aa8470f7196db22e
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-03-07 19:20:34 -0600

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import Article from "./Components/Article";
 
-const KoderList = [
-  {
-    "koderName": "Mickey Colorado",
-    "generation": "15g",
-    "status":"En curso"
-  },
-  {
-    "koderName": "Freedy",
-    "generation": "12g",
-    "status":"Egresado"
-  },
-  {
-    "koderName": "Erik Gutierrez",
-    "generation": "15g",
-    "status":"En curso"
-  },
-  {
-    "koderName": "Israel Salinas",
-    "generation": "1g",
-    "status":"Mentor"
-  }
-]
-
-
+import './App.css'
 
 function App() {
   return <div className='App'>
     <main>
-        <section>
-           {KoderList.map(koder => {
-             const {koderName, generation, status } = koder
-             return (
-               <Article 
-                  koderName= {koderName}
-                  generation= {generation}
-                  status= {status}
-               />
-             )
-           })}
-        </section>
+        
     </main>
 
   </div>
