d2265b7917eedb4cce01564d3ab3c2dc5d510e1d
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-03-11 13:07:59 -0600

    estados

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
 import { useState } from 'react'
 import HomeLight from './Components/HomeLight'
-import Formulario from './Components/H2'
-import H2 from './Components/H2'
-import { Card, ListGroup } from 'react-bootstrap'
+// import Formulario from './Components/H2'
+// import H2 from './Components/H2'
+// import { Card, ListGroup } from 'react-bootstrap'
 
 
 
 
 
 function App() {
-  // const [ isOn, setIsOn ] = useState( false )
-  // const toggleLight = () => {
+  // const [isOn, setIsOn] = useState(false)
+  // const toogleLight = ()  => {
   //   console.log(isOn)
   //   setIsOn(!isOn)
-  //   event.target.value
   // }
 
-    const [titleText, setTitleText] = useState (' ') 
-    const titleChange = (event) => {
-      console.log(event.target.value)
-      setTitleText(event.target.value)
-    }
+  const [title, setTitle] = useState(' ')
+
 
   return <div className='App'>
-    {/*<div className="home-light">
-  <HomeLight estaPrendido={isOn}/> 
-  <button onClick={toggleLight}>Conmutador</button>
-  </div>*/}
-            <input type="text" name="" id="Titulo" onChange={titleChange}/>
-            <H2  Title={titleText}/>
+          {/* <div className="home-light">
+            <HomeLight estaPrendido={isOn}/>
+            <button onClick={toogleLight}>Conmutador</button>
+          </div> */}
+          <div className="text">
+            <input type="text" />
+            <h2>Text: {Title} </h2>
           </div>
 
-
-
-  
-
-  return <div className='App'>
-    
-  </div>
-
-
+        </div>
 }
 
 export default App;
