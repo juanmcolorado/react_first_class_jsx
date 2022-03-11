dea9f3997d5c425014bba149edf08985a0c60e17
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-03-10 21:20:48 -0600

    clase react_5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import {useState} from 'react'
-import HomeLight from './Components/HomeLight'
-import Formulario from './Components/H2'
-import H2 from './Components/H2'
+import React from "react";
+import Card from './Components/Card'
 
 
 function App() {
-  // const [ isOn, setIsOn ] = useState( false )
-  // const toggleLight = () => {
-  //   console.log(isOn)
-  //   setIsOn(!isOn)
-  //   event.target.value
-  // }
-
-    const [titleText, setTitleText] = useState (' ') 
-    const titleChange = (event) => {
-      console.log(event.target.value)
-      setTitleText(event.target.value)
-    }
-
-  return <div className='App'>
-            {/*<div className="home-light">
-              <HomeLight estaPrendido={isOn}/> 
-              <button onClick={toggleLight}>Conmutador</button>
-              </div>*/}
-            <input type="text" name="" id="Titulo" onChange={titleChange}/>
-            <H2  Title={titleText}/>
-          </div>
+  return (
+    <div className='App'>
+      <Card />
+   </div>
+);
 }
 
 export default App;
